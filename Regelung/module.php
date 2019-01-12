6493a2c108ac02aa76d829f9021dd2408649757e
minor04 <46103212+minor04@users.noreply.github.com>
2019-01-12 19:46:03 +0100

    Update module.php

--- Regelung/module.php
+++ Regelung/module.php
@@ -197,6 +197,12 @@ class HeizungssteuerungRegler extends IPSModule
 		
 	public function ProgrammAuswahl(){
 		
+		$KategorieID_Heizung = IPS_GetCategoryIDByName("Heizung", 0);
+		$KategorieID_Settings = IPS_GetCategoryIDByName("Einstellungen", $KategorieID_Heizung);
+		$InstanzID = IPS_GetInstanceIDByName("Regler", $KategorieID_Settings);
+		IPS_ApplyChanges($deviceId);
+		//IPS_ApplyChanges($this->InstanceID);
+		
 		//$sws = $this->ReadPropertyInteger("SWS");
 		//$sws = getValue($this->GetIDForIdent("SWS"));
 		$zp_conf = getValue($this->GetIDForIdent("ZP_Conf"));
