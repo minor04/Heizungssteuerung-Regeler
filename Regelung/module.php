aa148e7fc057d832dfb914339616854b0d4bb4fa
minor04 <46103212+minor04@users.noreply.github.com>
2019-01-12 19:12:29 +0100

    Update module.php

--- Regelung/module.php
+++ Regelung/module.php
@@ -48,7 +48,7 @@ class HeizungssteuerungRegler extends IPSModule
 			//Timerzeit setzen in Minuten
 			$this->SetTimerInterval("UpdateWeather", $this->ReadPropertyInteger("UpdateWeatherInterval")*1000*60);
 			
-			$this->ProgrammAuswahl();
+			//$this->ProgrammAuswahl();
 			
 
         	}
@@ -317,7 +317,7 @@ class HeizungssteuerungRegler extends IPSModule
         		case 'SWS':
 			case 'SW':
 			$this->ApplyChanges();
-	    		//$this->ProgrammAuswahl();
+	    		$this->ProgrammAuswahl();
 	    		//$this->SetValue('SW_Ab', 0);
 	    		$value = $value;
 				
