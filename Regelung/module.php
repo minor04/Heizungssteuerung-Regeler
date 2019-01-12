fdeb9c2b8a0b95443cef6bc66dbf7cea2c5e9aa5
minor04 <46103212+minor04@users.noreply.github.com>
2019-01-12 19:07:56 +0100

    Update module.php

--- Regelung/module.php
+++ Regelung/module.php
@@ -198,20 +198,20 @@ class HeizungssteuerungRegler extends IPSModule
 	public function ProgrammAuswahl(){
 		
 		//$sws = getValue($this->GetIDForIdent("SWS"));
-		//$sws = $this->ReadPropertyInteger("SWS")
+		$sws = $this->ReadPropertyInteger("SWS")
 		$zp_conf = getValue($this->GetIDForIdent("ZP_Conf"));
 		$abw = getValue($this->GetIDForIdent("Abw"));
 		$test = getValue($this->GetIDForIdent("SWS_Abw"));
 		
-		IPS_ApplyChanges($this->GetIDForIdent("SWS"));
+		//IPS_ApplyChanges($this->GetIDForIdent("SWS"));
 		
-		if($this->ReadPropertyInteger("SWS") == 0){ //$sws == 0){
+		if($sws == 0){
 			SetValue($this->GetIDForIdent("prog"), 0);
 			IPS_SetDisabled($this->GetIDForIdent("prog"), false);
 			echo "0";
 		}
-		//else if($sws == 1){
-		else if($this->ReadPropertyInteger("SWS") == 1){
+		else if($sws == 1){
+		//else if($this->ReadPropertyInteger("SWS") == 1){
 			IPS_SetDisabled($this->GetIDForIdent("prog"), false);
 			echo "1";
 		}
