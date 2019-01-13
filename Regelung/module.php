92e348abd7af62f5d526df4454ade29e4dc58d72
minor04 <46103212+minor04@users.noreply.github.com>
2019-01-13 18:11:01 +0100

    Update module.php

--- Regelung/module.php
+++ Regelung/module.php
@@ -335,8 +335,8 @@ class HeizungssteuerungRegler extends IPSModule
 	public function Test(){
 		
 		//$this->EnableAction("SWS_Abw");
-		//$test = $this->getValue("SW");
-		$test = $this->ReadPropertyFloat("SW");
+		$test = $this->getValue("SW");
+		//$test = $this->ReadPropertyFloat("SW");
 		echo $test;
 		
 
@@ -345,7 +345,7 @@ class HeizungssteuerungRegler extends IPSModule
 	
 	public function Test2(){
 	
-		IPS_ApplyChanges(28875);
+		IPS_ApplyChanges(56821);
 	
 	}
 
